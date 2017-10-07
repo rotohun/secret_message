74a06eab2b219421764ea2055c6ec387337f30dc
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2017-10-07 14:11:25 -0400

    Function gets all the pictures names

---
+++ renamer.py
@@ -0,0 +1,13 @@
+import os
+
+
+
+def rename_files():
+    
+    # 1. Get file names from random folder.
+    file_list = os.listdir(r"/Users/dev/Desktop/Projects/python/secret_message/prank")
+    print(file_list)
+    
+    # 2. Rename file names.
+
+rename_files()
