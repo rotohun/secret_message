26b1a189a3553135050ba29b698b3030f4cfe174
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2017-10-07 14:36:51 -0400

    updated program for renaming pictures

--- renamer.py
+++ renamer.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 
 
 def rename_files():
@@ -7,7 +7,12 @@ def rename_files():
     # 1. Get file names from random folder.
     file_list = os.listdir(r"/Users/dev/Desktop/Projects/python/secret_message/prank")
     print(file_list)
+    saved_path = os.getcwd
+    os.chdir(r"/Users/dev/Desktop/Projects/python/secret_message/prank")
+
     
     # 2. Rename file names.
+    for file_name in file_list:
+        os.rename(file_name,re.sub('[0-9]', '', file_name))
 
 rename_files()
